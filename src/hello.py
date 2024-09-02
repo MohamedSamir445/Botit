70a0dfc136579d39d1e883fe6c8c54f96c16c19f
Mohamed SAMIR <100734578+MohamedSamir445@users.noreply.github.com>
2024-09-02 14:44:43 +0300

    edit code to check ci/cd

--- src/hello.py
+++ src/hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Botit!'
+    return 'Hello, Botit! from mohamed samir'
 
 @app.route('/<page_name>')
 def other_page(page_name):
